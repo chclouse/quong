886a4a3819ee12d76b469853eb156e9f8b8949b1
cloostefis <cloostefismt880@gmail.com>
2016-06-18 09:04:20 -0500

    Fixed: any key moves paddle down.

--- src/controller/keyboard_controller.py
+++ src/controller/keyboard_controller.py
@@ -4,6 +4,8 @@ import pygame
 
 class KeyboardController(Controller):
 
+	validKeys = (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
+
 	def __init__(self, connection):
 
 		self._connection = connection
@@ -11,8 +13,10 @@ class KeyboardController(Controller):
 
 	def update(self, update):
 
-		keyPressed = 0x4 if update.type == pygame.KEYDOWN else 0x0
-		keyDownRight = 0x2 if update.key in (pygame.K_DOWN, pygame.K_RIGHT) else 0x0
-		keyLeftUp = 0x1 if update.key in (pygame.K_UP, pygame.K_LEFT) else 0x0
+		if update.key in KeyboardController.validKeys:
 
-		self._connection.send(keyPressed | keyDownRight | keyLeftUp)
+			keyPressed = 0x4 if update.type == pygame.KEYDOWN else 0x0
+			keyDownRight = 0x2 if update.key in (pygame.K_DOWN, pygame.K_RIGHT) else 0x0
+			keyLeftUp = 0x1 if update.key in (pygame.K_UP, pygame.K_LEFT) else 0x0
+
+			self._connection.send(keyPressed | keyDownRight | keyLeftUp)
