ed8de7796ef2dd0f76cc3ce0b660df370af5a508
cloostefis <cloostefismt880@gmail.com>
2016-06-25 21:10:47 -0500

    THE BALL MOVES NOW!!!

--- src/entity/ball.py
+++ src/entity/ball.py
@@ -1,4 +1,5 @@
 from entity.entity import *
+import math
 import os
 import pygame
 import random
@@ -10,10 +11,12 @@ class Ball(Entity):
 
 		super(Ball, self).__init__(display)
 
+		self._speed          = 100
+		self._lastFrameTicks = pygame.time.get_ticks()
+		self._trajectory     = random.randint(0, 3) * 0.5 * math.pi
+
 		self.loadTexture()
 		self._x, self._y = display.width/2 - self._size[0], display.height/2 - self._size[1]
-		self._trajectory = random.choice((-1, 1)), random.choice((-1, 1))
-		self._rect.move_ip(self._x, self._y)
 
 
 	def loadTexture(self):
@@ -28,9 +31,18 @@ class Ball(Entity):
 
 		super(Ball, self).update()
 
+		t = pygame.time.get_ticks()
+		deltaTime = (t - self._lastFrameTicks) / 1000.0
+		self._lastFrameTicks = t
+
+		self._x = self._x + math.cos(self._trajectory)*deltaTime*self._speed
+		self._y = self._y + math.sin(self._trajectory)*deltaTime*self._speed
+
 
 	def draw(self, screen):
 
 		super(Ball, self).draw(screen)
 
+		self._rect.left = self._x
+		self._rect.top = self._y
 		screen.blit(self._texture, self._rect)
