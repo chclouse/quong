e81e00c66966f848ad6b2325f1d2fd83fc224c65
cloostefis <cloostefismt880@gmail.com>
2016-06-25 20:38:23 -0500

    Centered ball.

--- src/entity/ball.py
+++ src/entity/ball.py
@@ -1,6 +1,7 @@
 from entity.entity import *
 import os
 import pygame
+import random
 
 
 class Ball(Entity):
@@ -10,6 +11,9 @@ class Ball(Entity):
 		super(Ball, self).__init__(display)
 
 		self.loadTexture()
+		self._x, self._y = display.width/2 - self._size[0], display.height/2 - self._size[1]
+		self._trajectory = random.choice((-1, 1)), random.choice((-1, 1))
+		self._rect.move_ip(self._x, self._y)
 
 
 	def loadTexture(self):
@@ -17,6 +21,7 @@ class Ball(Entity):
 		directory = os.path.dirname(os.path.realpath(__file__))
 		self._texture =  pygame.image.load(directory + "/../../res/textures/ball.png")
 		self._rect    =  self._texture.get_rect()
+		self._size    =  self._texture.get_size()
 
 
 	def update(self):
